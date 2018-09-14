f08508a2cf87a4985f94251bc34aba3d6cd0e804
Unknown <martin.marmsoler@gmail.com>
2018-09-14 20:36:07 +0200

    removed default signals

--- src/signals.cpp
+++ src/signals.cpp
@@ -27,26 +27,6 @@
 
 Signals::Signals()
 {
-    // nur zum testen
-    struct Signal signal;
-
-    signal.name = "double a";
-    signal.datatype = "double";
-    signal.offset = 0;
-    signal.index = 0;
-    m_signals.append(signal);
-
-    signal.name = "float c";
-    signal.datatype = "float";
-    signal.offset = 8;
-    signal.index = 1;
-    m_signals.append(signal);
-
-    signal.name = "inta";
-    signal.datatype = "int";
-    signal.offset = 12; // anzahl bytes offset
-    signal.index = 2;
-    m_signals.append(signal);
 }
 
 void Signals::importSignals(){
